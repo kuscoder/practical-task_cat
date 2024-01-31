75085611d7f9b4f85cff252ac2568c84503bda70
kuscoder <kuscoder@yandex.ru>
2024-01-31 22:19:10 +0400

    Select component

--- src/components/shared/Select/Select.tsx
+++ src/components/shared/Select/Select.tsx
@@ -1,6 +1,41 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
+import { classNames } from 'utils/classNames'
+import { useFocusOut } from 'hooks/useFocusOut'
 import { SelectProps } from './Select.types'
+import chevronDown from 'public/assets/chevron-down.svg'
+import css from './Select.module.scss'
 
-export const Select: FC<SelectProps> = ({}) => {
-   return <></>
+export const Select: FC<SelectProps> = ({ className, placeholder, value, setValue, options }) => {
+   const [focus, setFocus] = useState(false)
+
+   const container = useFocusOut<HTMLDivElement>(focus, () => {
+      setFocus(false)
+   })
+
+   const chooseHandler = (value: string) => () => {
+      setFocus(false)
+      setValue(value)
+   }
+
+   return (
+      <div className={classNames(className, css.select, { [css.focus]: focus })} ref={container}>
+         <button className={classNames('focus-accent', css.value)} onFocus={() => setFocus(true)} type="button">
+            {value && <span>{value}</span>}
+            {!value && <span className={css.placeholder}>{placeholder}</span>}
+            <img src={chevronDown} alt="chevronDown" />
+         </button>
+
+         <ul className={classNames('scroll-y', css.options)}>
+            {options.map(option => (
+               <li className={css.option} key={option.value}>
+                  <button type="button" onClick={chooseHandler(option.value)}>
+                     {option.label}
+                  </button>
+               </li>
+            ))}
+
+            {!options.length && <li className={css.empty}>Nothing was found</li>}
+         </ul>
+      </div>
+   )
 }
--- src/components/shared/Select/Select.types.ts
+++ src/components/shared/Select/Select.types.ts
@@ -1,3 +1,12 @@
-export interface SelectProps {
+export interface Option {
+   value: string
+   label: string
+}
 
+export interface SelectProps {
+   className?: string
+   placeholder?: string
+   value: string | null
+   setValue: (value: string) => void
+   options: Option[]
 }

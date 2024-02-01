6fde6b03cc733e68f2bd7a7d4b088a6584b214c5
kuscoder <kuscoder@yandex.ru>
2024-02-01 12:02:32 +0400

    Animate Select dropdown block (.prettierrc mostly)

--- src/components/shared/Select/Select.tsx
+++ src/components/shared/Select/Select.tsx
@@ -1,6 +1,7 @@
 import { FC, useState } from 'react'
 import { classNames } from 'utils/classNames'
 import { useFocusOut } from 'hooks/useFocusOut'
+import { Flow } from '../Animations/Flow'
 import { SelectProps } from './Select.types'
 import chevronDown from 'public/assets/chevron-down.svg'
 import css from './Select.module.scss'
@@ -18,24 +19,45 @@ export const Select: FC<SelectProps> = ({ className, placeholder, value, setValu
    }
 
    return (
-      <div className={classNames(className, css.select, { [css.focus]: focus })} ref={container}>
-         <button className={classNames('focus-accent', css.value)} onFocus={() => setFocus(true)} type="button">
+      <div
+         className={classNames(className, css.select, { [css.focus]: focus })}
+         ref={container}
+      >
+         <button
+            className={classNames('focus-accent', css.value)}
+            onFocus={() => setFocus(true)}
+            type="button"
+         >
             {value && <span>{value}</span>}
             {!value && <span className={css.placeholder}>{placeholder}</span>}
-            <img src={chevronDown} alt="chevronDown" />
+            <img
+               src={chevronDown}
+               alt="chevronDown"
+            />
          </button>
 
-         <ul className={classNames('scroll-y', css.options)}>
-            {options.map(option => (
-               <li className={classNames(css.option, { [css.choosed]: option.value === value })} key={option.value}>
-                  <button type="button" onClick={chooseHandler(option.value)}>
-                     <span>{option.label}</span>
-                  </button>
-               </li>
-            ))}
+         <Flow
+            type="fade"
+            active={focus}
+         >
+            <ul className={classNames('scroll-y', css.options)}>
+               {options.map(option => (
+                  <li
+                     className={classNames(css.option, { [css.choosed]: option.value === value })}
+                     key={option.value}
+                  >
+                     <button
+                        type="button"
+                        onClick={chooseHandler(option.value)}
+                     >
+                        <span>{option.label}</span>
+                     </button>
+                  </li>
+               ))}
 
-            {!options.length && <li className={css.empty}>Nothing was found</li>}
-         </ul>
+               {!options.length && <li className={css.empty}>Nothing was found</li>}
+            </ul>
+         </Flow>
       </div>
    )
 }
